316990dcef33b58cd8423b0f7dc0fe0b4344e648
Lonely Guy <phoenex659@gmail.com>
2022-04-05 13:37:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import imgkit
+import ascii_magic
+
+vidObj = cv2.VideoCapture("./video.mp4")
+
+count = 0
+flag = 1
+while flag:
+    flag, image = vidObj.read()
+    try:
+        cv2.imwrite("./images/frame.jpg" % count, image)
+    except:
+        break
+    count += 1
+
+for i in range(count):
+    s = f"./images/frame.jpg{str(i)}.jpg"
+    output = ascii_magic.from_image_file(
+        s,
+        columns = 250,
+        width_ratio = 2,
+        mode = ascii_magic.Modes.HTML
+    )
+    ascii_magic.to_html_file(f"./html/frame{str(i)}.html", output, additional_styles='background: #222;')
+
+path = f"./my-secret-encoded.exe"
+config = imgkit.config(wkhtmltoimage=path)
+for i in range(count):
+    imgkit.from_file(f"./html/frame{str(i)}.html", f"./ascii/frame{str(i)}.jpg", config=config)
+
+frame = cv2.imread("./ascii/frame0.jpg")
+ih, iw, il = frame.shape
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter('./asciivideo.mp4', fourcc, 60, (iw, ih))
+
+for i in range(count):
+    image = f"./ascii/frame{str(i)}.jpg"
+    data = cv2.imread(image)
+    video.write(data)
+
+cv2.destroyAllWindows()
+video.release()
